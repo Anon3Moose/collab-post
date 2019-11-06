990078535cc7a20da7974f41a3b2689cf8d79611
Jared <34925072+Anon3Moose@users.noreply.github.com>
2019-11-05 17:53:33 -0800

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,4 +1,4 @@
--- Drops the blogger if it exists currently --
+-- Drops the database if it exists currently --
 DROP DATABASE IF EXISTS commentsdb;
--- Creates the "blogger" database --
+-- Creates the "comments" database (only temporary db) --
 CREATE DATABASE commentsdb;
